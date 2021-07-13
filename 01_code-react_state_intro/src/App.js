43b9cddc5ce33f2d258344bc906f9483b1d45782
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-07-13 17:08:12 +0200

    latest

--- 01_code-react_state_intro/src/App.js
+++ 01_code-react_state_intro/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, {useState} from 'react'
 import Counter from './components/Counter'
 
 
 
 function App() {
   /*let cappucino =7  everytime you render the function, cappucino will be re-rendered to 7 (not the new number), we therefore need to take the varaibles outside of the function */
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <Counter />
-
+      <h2>You spent {count} euros</h2>
+      <Counter ParentSetCount= {setCount} parentcount={count} counterName='tibidabo'/>
+      <Counter ParentSetCount= {setCount} parentcount={count} counterName='ovella negra'/> {/* Every time you create a new component, a new state is created - this means the 'count' will increase on either independently */}
     </div>
   );
 }
--- 01_code-react_state_intro/src/components/Counter.js
+++ 01_code-react_state_intro/src/components/Counter.js
@@ -1,3 +1,4 @@
+import { checkPropTypes } from 'prop-types';
 import React, {useState} from 'react' /* to achieve State we need to access a utility function - import by name 
 methods/ functions that start with 'use' are there to substitute class methods, they are called Hooks or utility functions
 This 'useState' solves the problem of using state*/
@@ -5,17 +6,21 @@ This 'useState' solves the problem of using state*/
 /* These functions must be used inside a react function component (so need a capital etc. i.e. function Counter()) */
 
 
-function Counter() {
-    const [count, setCount] = useState(0); /* stateArray has two elements: stateArray[0] is the value we store, stateArray[1] is the function that we use to change the value 
+function Counter(props) {
+    const count = props.parentCount;
+    const setCount = props.parentSetCount;
+    /* const [count, setCount] = useState(0); */ /* stateArray has two elements: stateArray[0] is the value we store, stateArray[1] is the function that we use to change the value 
     it can the be destructured to [count, setCount]
     userState takes one paramater, whatever goes inside is the value you get from stateArray[0] otherwise known as count when destructured, the first time that you run the function
     With react, it will only be executed one time and then change linked to our onClick callback functions*/
+    
+    /* in order to get the total of the two buttons, we need to add the useState to the parent element (see App.js), this is why const [count, setCount] above is starred out */
     return(
         <div>
-            <h2>Counter</h2>
+            <h2>Counter {props.counterName}</h2>
             <p>You clicked {count} times</p>
-            <button onClick={()=>count++}>Plus</button> {/* this is a callback function, first the event fires, then the entire function is re-rendered, meaning any let will be re-rendered and start from the original number, not the new number. This is why we need State */}
-            <button onClick={()=>count--}>Minus</button>
+            <button onClick={()=>setCount(count+1)}>Plus</button> {/* this is a callback function, first the event fires, then the entire function is re-rendered, meaning any let will be re-rendered and start from the original number, not the new number. This is why we need State */}
+            <button onClick={()=>setCount(count-1)}>Minus</button> {/* if you use a const for [count, setCount] then we can't use ++ as ++ will modify the original variable, which you can't with const */}
         </div>
 
     )
